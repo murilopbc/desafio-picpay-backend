83c349d1bb939b96da3426cfe93da40af117a948
murilopbc <murilopoltronieri3@gmail.com>
2024-08-01 13:18:31 -0300

    h2 config

--- src/main/resources/db/migration/V2__create-table-transactions.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE transactions (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    amount DECIMAL(10, 2),
-    sender_id BIGINT,
-    receiver_id BIGINT,
-    data_hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (sender_id) REFERENCES users(id),
-    FOREIGN KEY (receiver_id) REFERENCES users(id)
-);
